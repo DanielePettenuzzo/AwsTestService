378b651b1c088119b55574b1d294bcdc52162e58
DanielePettenuzzo <daniele@px4.io>
2023-08-06 14:28:41 +0200

    feat: add simple rest server

--- apps/awesome-app/main.cpp
+++ apps/awesome-app/main.cpp
@@ -1,7 +1,16 @@
 #include <cpp-template/cpp_template.hpp>
+#include <restinio/all.hpp>
 
 int main(int /* argc */, char** /* argv */)
 {
-  cpp_template::CppTemplate cpp_template;
-  return cpp_template.getNumber();
+  std::cout << "Hello, World!" << std::endl;
+  try {
+    restinio::run(
+        restinio::on_this_thread().port(8080).address("localhost").request_handler([](auto req) {
+          std::cout << "Request..." << std::endl;
+          return req->create_response().set_body("Hello, World!").done();
+        }));
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
 }
